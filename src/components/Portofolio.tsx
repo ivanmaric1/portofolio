cb2d7fd6913faf5838369b0fa65bbfe318b80c4c
Ivan <js314head@gmail.com>
2020-12-07 07:43:29 +0100

    Početak projekta

---
+++ src/components/Portofolio.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Work from './Work';
+import Contact from './Contact';
+
+const Portofolio = () => {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Work />
+      <Contact />
+    </div>
+  );
+};
+
+export default Portofolio;
